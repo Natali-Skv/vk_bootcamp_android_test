131c9bd5df817bb89c5011e45e3de6e7a9ee496b
Natali-Skv <nat-s.skv@mail.ru>
2022-07-16 03:56:43 +0300

    мини рефакторинг

--- app/src/main/java/com/sirius/test_app/AppDetailsActivity.kt
+++ app/src/main/java/com/sirius/test_app/AppDetailsActivity.kt
@@ -1,66 +1,71 @@
 package com.sirius.test_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sirius.test_app.databinding.AppDetailsBinding
 
 class AppDetailsActivity : AppCompatActivity() {
     private lateinit var bindingClass: AppDetailsBinding
-    private val currAppData=DataModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingClass=AppDetailsBinding.inflate(layoutInflater)
+        bindingClass = AppDetailsBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
+        val currAppData = DataModel()
         bindingClass.banner.setImageResource(currAppData.image)
         bindingClass.logo.setImageResource(currAppData.logo)
-        bindingClass.appTitle.text=currAppData.name
-        bindingClass.appUsers.text=currAppData.gradeCnt
-        bindingClass.description.text=currAppData.description
-        bindingClass.rating.text=currAppData.rating.toString().subSequence(0,3)
-        bindingClass.reviewsCount.text=currAppData.gradeCnt
+        bindingClass.appTitle.text = currAppData.name
+        bindingClass.appUsers.text = currAppData.gradeCnt
+        bindingClass.description.text = currAppData.description
+        bindingClass.rating.text = currAppData.rating.toString().subSequence(0, 3)
+        bindingClass.reviewsCount.text = currAppData.gradeCnt
 
-        if (currAppData.rating < 4.5) {
-            bindingClass.star5.setColorFilter(R.color.gray)
-            bindingClass.headerStar5.setColorFilter(R.color.gray)
-        }
-        if (currAppData.rating < 3.5) {
-            bindingClass.star4.setColorFilter(R.color.gray)
-            bindingClass.headerStar4.setColorFilter(R.color.gray)
-        }
-        if (currAppData.rating < 2.5) {
-            bindingClass.star3.setColorFilter(R.color.gray)
-            bindingClass.headerStar3.setColorFilter(R.color.gray)
-        }
-        if (currAppData.rating < 1.5) {
-            bindingClass.star2.setColorFilter(R.color.gray)
-            bindingClass.headerStar2.setColorFilter(R.color.gray)
-        }
-        if (currAppData.rating < 0.5)  {
-            bindingClass.star1.setColorFilter(R.color.gray)
-            bindingClass.headerStar1.setColorFilter(R.color.gray)
-        }
+        setRatingStars(currAppData.rating)
+        createTags(currAppData.tags)
+        createReviewsList(currAppData.reviews)
+    }
 
-
-        for (tagText in currAppData.tags) {
+    private fun createTags(tags:List<String>) {
+        for (tagText in tags) {
             val tagView = TextView(this, null, 0, R.style.tag)
             tagView.text = tagText
             tagView.id = View.generateViewId()
             bindingClass.rootLayout.addView(tagView)
             bindingClass.tags.addView(tagView)
         }
-
-        createReviewsList()
-
     }
-    private fun createReviewsList(){
+
+    private fun createReviewsList(reviews:List<ReviewModel>) {
         val linearLayoutManager = LinearLayoutManager(applicationContext)
         linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         bindingClass.reviewsRV.layoutManager = linearLayoutManager
-        val adapter = ReviewsAdapter(currAppData.reviews)
+        val adapter = ReviewsAdapter(reviews)
         bindingClass.reviewsRV.adapter = adapter
+    }
+
+    private fun setRatingStars(rating:Float) {
+        if (rating < 4.5) {
+            bindingClass.star5.setColorFilter(R.color.gray)
+            bindingClass.headerStar5.setColorFilter(R.color.gray)
+        }
+        if (rating < 3.5) {
+            bindingClass.star4.setColorFilter(R.color.gray)
+            bindingClass.headerStar4.setColorFilter(R.color.gray)
+        }
+        if (rating < 2.5) {
+            bindingClass.star3.setColorFilter(R.color.gray)
+            bindingClass.headerStar3.setColorFilter(R.color.gray)
+        }
+        if (rating < 1.5) {
+            bindingClass.star2.setColorFilter(R.color.gray)
+            bindingClass.headerStar2.setColorFilter(R.color.gray)
+        }
+        if (rating < 0.5) {
+            bindingClass.star1.setColorFilter(R.color.gray)
+            bindingClass.headerStar1.setColorFilter(R.color.gray)
+        }
     }
 }
